e8c0d72db3aebf0789054859e1719e6bcd0cc0e0
kangzj <kangzj@users.noreply.github.com>
2022-04-12 22:11:44 +0000

    Search: add class to retrieve search product pricing (#23892)
    * [not verified] added class
    
    * [not verified] applied coupon
    
    * [not verified] replace assertEqualsWithDelta with assertEquals for older phpunit
    
    * add estimated_count
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2157799000

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -106,6 +106,7 @@ class ComposerStaticInitb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ0_1_0_
         'Automattic\\Jetpack\\Search\\Options' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-options.php',
         'Automattic\\Jetpack\\Search\\Package' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-package.php',
         'Automattic\\Jetpack\\Search\\Plan' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-plan.php',
+        'Automattic\\Jetpack\\Search\\Product' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-product.php',
         'Automattic\\Jetpack\\Search\\REST_Controller' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-rest-controller.php',
         'Automattic\\Jetpack\\Search\\Search_Widget' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/widgets/class-search-widget.php',
         'Automattic\\Jetpack\\Search\\Settings' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-search/src/class-settings.php',
