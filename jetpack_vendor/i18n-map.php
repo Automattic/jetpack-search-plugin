2eeaed08f795bad4e35e74bebb3041c11abe579e
samiff <samiff@users.noreply.github.com>
2023-01-23 19:08:39 +0000

    Jetpack: backport 11.8-a.9 release (#28539)
    * Changelog edits for plugins/jetpack
    
    * Jetpack: backport 11.8-a.9 release
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3989541209

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.2.38-alpha',
+      'ver' => '2.2.38',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.7.5-alpha',
+      'ver' => '2.7.5',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.31.4-alpha',
+      'ver' => '0.31.4',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.47.1-alpha',
+      'ver' => '1.47.1',
     ),
   ),
 );
