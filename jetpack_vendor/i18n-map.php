451e725a282249a2bf0c2df67461c55485efab6a
anomiex <anomiex@users.noreply.github.com>
2023-02-20 09:35:29 +0000

    Remove remnants of automated code coverage support (#28982)
    PR #28542 removed the calls to the coverage scripts (except for one
    line), but didn't update the documentation, remove the scripts
    themselves, or adjust `jetpack generate` to stop generating those
    scripts. This does all that.
    
    It does not remove the PHPUnit or Jest definitions of which files should
    be looked at for coverage, or any `@codeCoverageIgnore` directives, or
    the `test-coverage-html` script in packages/waf, as those may still be
    useful for people manually looking at coverage locally.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4221844145

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.16',
+      'ver' => '0.2.17-alpha',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.17.31',
+      'ver' => '1.17.32-alpha',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.8.37',
+      'ver' => '0.8.38-alpha',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.8.0',
+      'ver' => '1.8.1-alpha',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
@@ -38,11 +38,11 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.2.10',
+      'ver' => '0.2.11-alpha',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3-alpha',
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
@@ -50,11 +50,11 @@ return array(
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.5.0',
+      'ver' => '0.5.1-alpha',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.47.1',
+      'ver' => '1.47.2-alpha',
     ),
   ),
 );
