5bac42d889fff658b0598c08dd1c70c1cc4674e7
CodeyGuyDylan <CodeyGuyDylan@users.noreply.github.com>
2023-07-20 19:05:15 +0000

    Update project versions for unintentional major update (#31978)
    * [not verified] changelog
    
    * [not verified] Fixup project versions
    
    * [not verified] Unedit protect
    
    * [not verified] Fix syntax error
    
    * [not verified] Fixup project versions
    
    * [not verified] remove outdated changelogs
    
    * changelog
    
    * remove uncessary changelog
    
    * Remove protect changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5614779619

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ2_0_0_alpha::getLoader();
+return ComposerAutoloaderInitb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ1_5_0_alpha::getLoader();
