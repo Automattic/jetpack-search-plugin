dab1be111553389ae88a1c0110744a8193039ebb
lezama <lezama@users.noreply.github.com>
2023-09-05 20:14:49 +0000

    Paywall: Add login link to simple sites (#32849)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6089117871

--- jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'be05f4fa6476173ae8aa');
--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd4e5bcafc472e51901e3');
