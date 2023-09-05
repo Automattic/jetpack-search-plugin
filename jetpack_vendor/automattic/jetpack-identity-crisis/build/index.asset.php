1ee11441d2c02ea142f012e31da32ff87533330b
edanzer <edanzer@users.noreply.github.com>
2023-09-05 23:08:39 +0000

    Subscriptions: Hide modal on protected posts (#32867)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6090524154

---
+++ jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'be05f4fa6476173ae8aa');
