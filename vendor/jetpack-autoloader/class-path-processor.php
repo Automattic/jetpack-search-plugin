a318f9a5b3aaf17b9dbe6db28e076721510ac6ad
jeherve <jeherve@users.noreply.github.com>
2023-11-21 17:05:37 +0000

    Remove unnecessary `function_exists()` checks (#34222)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6947021022

--- vendor/jetpack-autoloader/class-path-processor.php
+++ vendor/jetpack-autoloader/class-path-processor.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ2_0_0_alpha\al3_0_1;
+namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ2_0_0_alpha\al3_0_2_alpha;
 
  // phpcs:ignore
 
