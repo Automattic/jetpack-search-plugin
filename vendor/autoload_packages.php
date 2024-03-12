ddb720f5399f7efc286f532798404b9fbd0b79f9
anomiex <anomiex@users.noreply.github.com>
2024-03-12 20:08:33 +0000

    Phan: Add phan configs to all php projects (#36332)
    Per pdWQjU-IH-p2, we're going to use Phan for PHP static analysis.
    
    This commit adds Phan configs to all the php-using projects, and to new
    projects created with `jetpack generate`.
    
    It also adds a global production-exclude for the `.phan/` directory, as
    we'll never want that in mirror repos.
    
    And finally, it adds a structure test to ensure php-using projects
    continue to have phan configs.
    
    ---------
    
    Co-authored-by: Igor Zinovyev <zinigor@gmail.com>
    Co-authored-by: Steve D <33553323+sdixon194@users.noreply.github.com>
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8254939655

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ2_1_0_alpha\al3_0_2;
+namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ2_1_0_alpha\al3_0_3_alpha;
 
  // phpcs:ignore
 
