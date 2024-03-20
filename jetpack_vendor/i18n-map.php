b24c051c1919d417b0178041fae4aa8698086b19
anomiex <anomiex@users.noreply.github.com>
2024-03-20 13:20:22 +0000

    phan: Add stubs for WordPress-defined constants (#36470)
    Unfortunately the php-stubs/wordpress-globals package is
    broken-as-designed, because the maintainer doesn't actually want anyone
    to use it. Instead he wants people to use PHPStan, which needs a
    bootstrap file for constants, which he provides in his PHPStan plugin.
    
    This initial version copies most of the constants from that PHPStan
    plugin's bootstrap.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8359449855

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,11 +14,11 @@ return array(
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.2.5',
+      'ver' => '0.2.6-alpha1710939962',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.3.7',
+      'ver' => '0.3.8-alpha1710939962',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.6.0-alpha1710788061',
+      'ver' => '2.6.0-alpha1710939962',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.17.4',
+      'ver' => '0.17.5-alpha1710939962',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.1',
+      'ver' => '3.1.2-alpha1710939962',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.18.0-alpha1710858286',
+      'ver' => '4.18.0-alpha1710939962',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,19 +54,19 @@ return array(
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.3.2',
+      'ver' => '0.3.3-alpha1710939962',
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.43.6',
+      'ver' => '0.43.7-alpha1710939962',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.11.1',
+      'ver' => '0.11.2-alpha1710939962',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.10.2',
+      'ver' => '2.10.3-alpha1710939962',
     ),
   ),
 );
