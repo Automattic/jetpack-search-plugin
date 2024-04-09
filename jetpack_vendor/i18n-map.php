1aec94f758583a87b1092822bd8b92b2e1b272a9
anomiex <anomiex@users.noreply.github.com>
2024-04-09 15:42:59 +0000

    Backport mu-wpcom-plugin 2.1.14, jetpack 13.4-a.1 Changes (#36792)
    Changelog and readme.txt edits.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8618336925

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.3.10-alpha1712563319',
+      'ver' => '0.3.10',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.1-alpha1712563319',
+      'ver' => '2.7.1',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.18.3-alpha1712563319',
+      'ver' => '0.18.3',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,15 +38,15 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.4-alpha1712663009',
+      'ver' => '3.1.5-alpha1712676493',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '2.0.4-alpha1712563319',
+      'ver' => '2.0.4',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.21.0-alpha1712614863',
+      'ver' => '4.21.1-alpha1712676493',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,19 +54,19 @@ return array(
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.3.4-alpha1712563319',
+      'ver' => '0.3.4',
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.44.0-alpha1712563319',
+      'ver' => '0.44.0',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.12.1-alpha1712563319',
+      'ver' => '0.12.1',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.11.0-alpha1712663009',
+      'ver' => '2.11.1-alpha1712676493',
     ),
   ),
 );
