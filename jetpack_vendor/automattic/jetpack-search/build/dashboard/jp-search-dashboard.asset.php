5c9db2f077fa73e2ba58962ab02b12e2e6f03298
nateweller <nateweller@users.noreply.github.com>
2024-11-06 17:07:04 +0000

    Components: Add optional hover support to the IconTooltip component (#39916)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11708168208
    
    Upstream-Ref: Automattic/jetpack@449cd078f43e97a018fd9385207abc7eaa37050a

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '616a63f49a05902827f1');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '445bb0f7f161bb5402c0');
