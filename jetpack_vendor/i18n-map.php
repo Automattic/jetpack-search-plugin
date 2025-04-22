d30b5fe3fc97f70f1a5929541a648ba1d5791d18
anomiex <anomiex@users.noreply.github.com>
2025-04-22 20:02:41 +0000

    phpcs: Fix and enable `Jetpack.PHPUnit.Attributes` sniff (#43175)
    Mostly this is the result of running phpcbf on files failing the sniff.
    
    Things needing manual cleanup were:
    
    * `projects/packages/image-cdn/tests/php/Image_CDN_Test.php`:
      Remove a broken `@covers` that had no space between the tag and class
      name.
    
    * `projects/plugins/jetpack/tests/php/core-api/WPCOM_REST_API_V2_Field_Controller_Test.php`:
      Fix a `@group` that was accidentally
      "rest-api\n\nphpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound".
    
    * `projects/plugins/jetpack/tests/php/core-api/wpcom-fields/WPCOM_REST_API_V2_Attachment_VideoPress_Data_Test.php`:
    * `projects/plugins/jetpack/tests/php/sync/Jetpack_Sync_Queue_TestBase.php`:
      Move some `@group`s from the file comment to class comment.
    
    * `projects/plugins/jetpack/tests/php/Functions_OpenGraph_Test.php`:
    * `projects/plugins/jetpack/tests/php/general/Jetpack_Client_Server_Test.php`:
      Fix where it got confused about whether a comment was a file or class
      comment.
    
    * `projects/plugins/jetpack/tests/php/json-api/Jetpack_Json_Api_Plugins_Endpoints_Test.php`:
      Fix `@covers` for nonexistent class.
    
    * `projects/plugins/jetpack/tests/php/modules/shortcodes/trait.http-request-cache.php`:
    * `projects/plugins/jetpack/tests/php/trait-woo-tests.php`:
      Manually fix, since the sniff doesn't recognize the trait as a test
      class.
    
    * Disable `PHPCompatibility.Attributes.NewAttributes.PHPUnitAttributeFound` in phpcompatibility-dev run.
    
    `PHPCompatibility.Attributes.NewAttributes.Found` was already disabled.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14603283610
    
    Upstream-Ref: Automattic/jetpack@c49fe6886554654995d9d453e5191b2e5cb28a61

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.7',
+      'ver' => '0.5.8-alpha1745351100',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.18',
+      'ver' => '4.0.19-alpha1745351100',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.7',
+      'ver' => '0.4.8-alpha1745351100',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.0',
+      'ver' => '6.11.1-alpha1745351100',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -34,11 +34,11 @@ return array(
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.8',
+      'ver' => '0.4.9-alpha1745351100',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.13',
+      'ver' => '4.2.14-alpha1745351100',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,11 +46,11 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.12.1-alpha1745322706',
+      'ver' => '5.12.1-alpha1745351100',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.7',
+      'ver' => '0.4.8-alpha1745351100',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -62,19 +62,19 @@ return array(
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.9',
+      'ver' => '0.5.10-alpha1745351100',
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.47.22',
+      'ver' => '0.47.23-alpha1745351100',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.16.1',
+      'ver' => '0.16.2-alpha1745351100',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.11.0',
+      'ver' => '4.11.1-alpha1745351100',
     ),
   ),
 );
