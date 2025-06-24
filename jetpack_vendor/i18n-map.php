58d50769884b61abd3d2f479773b402d80d50aef
tbradsha <tbradsha@users.noreply.github.com>
2025-06-24 13:30:20 +0000

    Changelog and readme.txt edits. (#44061)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15851444438
    
    Upstream-Ref: Automattic/jetpack@8470dea24b54e954d89598f72ca9c4d1479aaca8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.8-alpha1750695105',
+      'ver' => '6.13.8',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.16.5',
+      'ver' => '5.16.6',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.52.3-alpha1750347518',
+      'ver' => '0.52.3',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.17.1-alpha1750695105',
+      'ver' => '0.17.1',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
