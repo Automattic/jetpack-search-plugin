d86e16d40037c68f39ea56c99ab2cfde762cce78
tbradsha <tbradsha@users.noreply.github.com>
2025-09-11 20:38:27 +0000

    Clean up various common typos in the monorepo (#45148)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17656336763
    
    Upstream-Ref: Automattic/jetpack@92b9b35201a27203cbbc7109921d81376cd5a6d8

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_1_0\al5_0_9;
+namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_1_0\al5_0_10_alpha;
 
  // phpcs:ignore
 
