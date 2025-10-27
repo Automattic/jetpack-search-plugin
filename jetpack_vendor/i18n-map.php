afa8787ade1099555a52294e0b3ccee980986020
tbradsha <tbradsha@users.noreply.github.com>
2025-10-27 16:04:39 +0000

    Backport jetpack 15.2-a.7 Changes (#45635)
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18847089469
    
    Upstream-Ref: Automattic/jetpack@dd86ce2a5f688ed5c8e7a6bea8c5e347a50bdac3

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.1-alpha1761311430',
+      'ver' => '6.19.1',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.27.9-alpha1761327119',
+      'ver' => '5.27.9',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.53.0-alpha1761237364',
+      'ver' => '0.53.0',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.22.2-alpha1761311430',
+      'ver' => '4.22.2',
     ),
   ),
 );
