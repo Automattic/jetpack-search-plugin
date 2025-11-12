2e14a3e04460c505719c97fe75f249103e768963
tbradsha <tbradsha@users.noreply.github.com>
2025-11-12 16:58:51 +0000

    Updated package versions for automattic/jetpack-search-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19305225375
    
    Upstream-Ref: Automattic/jetpack@2151487cca66ff255815fb200906aa43973d1736

--- vendor/jetpack-autoloader/class-latest-autoloader-guard.php
+++ vendor/jetpack-autoloader/class-latest-autoloader-guard.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_2_0_alpha\al5_0_13_alpha;
+namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_2_1\al5_0_13;
 
  // phpcs:ignore
 
