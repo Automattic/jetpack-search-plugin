2928d261eb3b87bc423010dd8f5a769bab635ad8
anomiex <anomiex@users.noreply.github.com>
2025-12-11 19:16:34 +0000

    tests: Fix PHPUnit 12.5 whining about mocks versus stubs (#46260)
    PHPUnit 12.5 has started complaining about the use of mocks when stubs
    would be sufficient. Unfortunately the ability to create partial stubs
    was only added in 12.5.1, so in many cases we have to just set the
    attribute to ignore the complaints.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20144146632
    
    Upstream-Ref: Automattic/jetpack@6e2cd72b779a605c61bed1deb4c86eb210a5dc42

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_2_3_alpha\al5_0_14;
+namespace Automattic\Jetpack\Autoloader\jpb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ5_2_3_alpha\al5_0_15_alpha;
 
  // phpcs:ignore
 
