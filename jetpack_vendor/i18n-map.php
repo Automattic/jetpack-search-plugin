2eb294d702845c41be9c6abc7723fe1a42cd03db
anomiex <anomiex@users.noreply.github.com>
2026-01-16 21:53:49 +0000

    Update wordpress monorepo (#46647)
    * Update wordpress monorepo
    * Remove no-longer-needed patches.
      Still need a few for `@wordpress/dataviews`, as that's upgraded in a
      separate PR.
    * We can also remove setting `resolve.fullySpecified: false` for GB packages now.
      Again, except when `@wordpress/dataviews` is involved.
    * Fix jetpack static build i18n-to-php include
    * Bump some `@types/react` versions, weird mismatch error
    * charts: Fix design tokens in a story, new eslint sniff.
    * pnpmfile hack for `@wordpress/sync` → `diff` now too
    * Pnpmfile hack for `@wordpress/jest-console` → jest 29 too
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21081573052
    
    Upstream-Ref: Automattic/jetpack@d889e081300f60e4d1b1d87b5cdf8f9b4bfaad7a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.19',
+      'ver' => '4.3.20-alpha1768599302',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.20.3-alpha1768595616',
+      'ver' => '6.20.3-alpha1768599302',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.6',
+      'ver' => '0.4.7-alpha1768599302',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.15',
+      'ver' => '4.3.16-alpha1768599302',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.30.1-alpha1768427895',
+      'ver' => '5.30.1-alpha1768599302',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.55.0-alpha1768326783',
+      'ver' => '0.55.0-alpha1768599302',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
