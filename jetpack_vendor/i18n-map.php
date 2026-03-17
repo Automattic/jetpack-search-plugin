e89046871b83f9daaed64c9a78f7fe50f16bee87
tbradsha <tbradsha@users.noreply.github.com>
2026-03-17 20:20:52 +0000

    Backport jetpack 15.7-a.3 changes (#47606)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23213853008
    
    Upstream-Ref: Automattic/jetpack@d2312a87aabc50ecc8ee4fb96ea2f32cb187665e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.16-alpha1773069364',
+      'ver' => '0.4.16',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.26-alpha1773069364',
+      'ver' => '4.3.26',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.32.0-alpha1773699760',
+      'ver' => '5.33.0-alpha1773777635',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.56.4-alpha1773069364',
+      'ver' => '0.56.4',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.33.0-alpha1773760813',
+      'ver' => '4.33.0-alpha1773777635',
     ),
   ),
 );
