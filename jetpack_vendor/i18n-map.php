aa90e6f7427f3fac6bb051ebd3ea4eb00ff38e5d
anomiex <anomiex@users.noreply.github.com>
2026-05-20 19:08:07 +0000

    Update wordpress monorepo (#48405)
    * Update wordpress monorepo
    * Fix copy-pasted styles in search to match WordPress/gutenberg/77717
    * search: Also remove styles removed in WordPress/gutenberg/59096
    * components: Simplify buttons in footer and update tests
    * Bump eslint-plugin-react-hooks version
    * Fix tests failing due to uuid v14 going esm-only
    * Fix wp-build-polyfills test to not incorrectly fail on the dev build
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Mikael Korpela <mikael.korpela@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26183087836
    
    Upstream-Ref: Automattic/jetpack@94ba7494ccb69fc1b462262e89528e32db6b2821

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.5',
+      'ver' => '0.8.6-alpha1779302993',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.38-alpha1779296686',
+      'ver' => '4.3.38-alpha1779302993',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.7-alpha1779215895',
+      'ver' => '8.3.7-alpha1779302993',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.26-alpha1779296686',
+      'ver' => '0.4.26-alpha1779302993',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.35',
+      'ver' => '4.3.36-alpha1779302993',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.37.3-alpha1779296686',
+      'ver' => '5.37.3-alpha1779302993',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.61.0-alpha1779296686',
+      'ver' => '0.61.0-alpha1779302993',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
