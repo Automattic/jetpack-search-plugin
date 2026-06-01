31fb7210ff2aa8a372897248c1be40fd0be7c9d9
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-06-01 07:26:18 +0000

    Backport social 9.0.0 changes (#49301)
    * Changelog and readme.txt edits.
    
    * Version bumps
    
    * Update stable tag in readme.txt
    
    * Update composer.lock files
    
    * Another attempt to update lockfiles
    
    * Remove non-user facing changelog entries from readme.txt
    
    * One more attempt to fix lockfiles
    
    * One final attempt to fix the lockfiles
    
    * Update them one more time
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26740518364
    
    Upstream-Ref: Automattic/jetpack@318429cc2d195a0a36f57ab625e563439adbfe40

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.7-alpha1780063295',
+      'ver' => '0.8.7',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.0-alpha1780045796',
+      'ver' => '8.5.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.37-alpha1779978991',
+      'ver' => '4.3.37',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.37.5-alpha1780096980',
+      'ver' => '5.37.5',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -74,11 +74,11 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.39.0-alpha1780041044',
+      'ver' => '4.39.0',
     ),
     'jetpack-wp-abilities' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-abilities',
-      'ver' => '0.1.2-alpha1779942968',
+      'ver' => '0.1.2',
     ),
   ),
 );
