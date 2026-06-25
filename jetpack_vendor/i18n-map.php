af1c7dff78356389fa613ebd9a97d50e0581e809
anomiex <anomiex@users.noreply.github.com>
2026-06-25 19:46:25 +0000

    Update babel configs in preparation for Babel 8 (#49977)
    We're not quite ready to actually update to Babel 8, but we can go ahead
    with the things recommended at https://babeljs.io/docs/v8-migration.
    
    * `@babel/preset-env`: Set `bugfixes: true` to match Babel 8 behavior.
    * `@babel/preset-react`: Set `runtime: 'classic'` and `useSpread: true`
      for the few things using Preact. Babel 8 is changing the default
      runtime.
    * `@babel/preset-typescript`: Set `allowDeclareFields: true` to match
      Babel 8 behavior.
    * Babel 8 is moving `targets` from `@babel/preset-env` etc to the top
      level. Adjust the configs accordingly.
    * Set `targets` in `babel.config.js` where the project wasn't already
      setting a browserslist configuration, as babel's default is changing
      from "all browsers" to "browserslist defaults".
    * packages/search: Use `babel-plugin-polyfill-corejs3` directly, as
      `@babel/preset-env` is removing the `corejs` option in Babel 8.
    * plugins/inspect: Use standard babel config. The one it had seems like
      it was wrong (`targets: { node: 'current' }`?).
    * Clean up some unnecessary project-level babel configs.
      * Deleting various `*/.babelrc` seems to make no difference to
        anything.
      * Jetpack's `babel.config.js` had a bunch of complication that seems
        to make no difference.
    * Cleaned up unused babel deps in projects.
    
    As far as build output, the changes should be minor:
    `allowDeclareFields` explicitly sets some class fields to undefined, and
    `useSpread` has Babel use `...` in some React transforms. Other than
    that, things should generally be the same as before.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28195132223
    
    Upstream-Ref: Automattic/jetpack@a72d6efab88397f7e703e1992d7879de0a97c469

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.5.2',
+      'ver' => '0.5.3-alpha1782415648',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.4',
+      'ver' => '8.7.5-alpha1782415648',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.32',
+      'ver' => '0.4.33-alpha1782415648',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.3-alpha1782393456',
+      'ver' => '5.40.3-alpha1782415648',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '7.2.5',
+      'ver' => '7.2.6-alpha1782415648',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
