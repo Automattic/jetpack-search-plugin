487567dce7144d57e02d0b717238d988eaf58644
simison <simison@users.noreply.github.com>
2026-07-10 11:23:14 +0000

    Search: remove `calypso-color-schemes` and `postcss-custom-properties` plugin (#50276)
    * Remove calypso-color-schemes and postcss-custom-properties plugin
    * Add wpds token fallbacks at build time
    * Remove separate block config
    * Run PostCSS on Search Dashboard
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29088406628
    
    Upstream-Ref: Automattic/jetpack@2433e388c20d737a1fd9c9d100c12c0eb37a1998

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '7.2.8-alpha1783426613',
+      'ver' => '7.2.8-alpha1783681588',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
