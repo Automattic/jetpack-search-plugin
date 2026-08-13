d4a4ecc85a9c7ad9a3076dbdb2707e6db711f475
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-08-13 19:00:52 +0000

    Updated package versions for automattic/jetpack-search-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31733244439
    
    Upstream-Ref: Automattic/jetpack@900611a34504f00f0ac65196a49cbfe1e2d0349d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.10.2-alpha1786640982',
+      'ver' => '8.10.2',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.43.0-alpha1786625991',
+      'ver' => '5.43.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,11 +78,11 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '7.4.0-alpha1786585409',
+      'ver' => '7.4.0',
     ),
     'jetpack-stats-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.20.2-alpha1786489516',
+      'ver' => '0.20.2',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
