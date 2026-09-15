8c3f57befa0bfe6f844d246e91985aef3fda92d3
enejb <enejb@users.noreply.github.com>
2026-09-15 16:04:05 +0000

    My Jetpack: add the Main_Features catalog with verified copy and links (#52295)
    * My Jetpack: add the Main_Features catalog of main Jetpack features
    
    * My Jetpack: list each feature's WordPress.org plugin URL in the catalog
    
    * My Jetpack: align Jetpack AI Assistant copy with jetpack.com/ai
    
    * My Jetpack: highlight multistep forms and secure file uploads in the Forms copy
    
    * My Jetpack: simplify the Forms copy around range and secure file uploads
    
    * My Jetpack: reduce Main_Features to the feature definitions
    
    * My Jetpack: address review feedback on the Main_Features catalog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34990236581
    
    Upstream-Ref: Automattic/jetpack@9f6a3b8ea3132f32e49014823eb9100a1ec704e3

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -132,6 +132,7 @@ class ComposerStaticInitb462338fb66be23595d68a93345c9e3d_jetpack_searchⓥ8_0_0_
         'Automattic\\Jetpack\\My_Jetpack\\Hybrid_Product' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-hybrid-product.php',
         'Automattic\\Jetpack\\My_Jetpack\\Initializer' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-initializer.php',
         'Automattic\\Jetpack\\My_Jetpack\\Jetpack_Manage' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-jetpack-manage.php',
+        'Automattic\\Jetpack\\My_Jetpack\\Main_Features' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-main-features.php',
         'Automattic\\Jetpack\\My_Jetpack\\Menu_Visibility' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-menu-visibility.php',
         'Automattic\\Jetpack\\My_Jetpack\\Module_Product' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-module-product.php',
         'Automattic\\Jetpack\\My_Jetpack\\Product' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-product.php',
