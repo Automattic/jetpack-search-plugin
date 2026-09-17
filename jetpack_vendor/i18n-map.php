0924220a46a708873ee5dd3699dbf32d5792850f
anomiex <anomiex@users.noreply.github.com>
2026-09-17 19:21:25 +0000

    Update dependency prettier to v3.9.6 (#52356)
    * Update dependency prettier to v3.9.6
    * Fix code in `.github/renovate-config.js` that prettier now breaks.
    * Configure to not add trailing commas to `.jsonc` files.
    * Run `pnpm lint-required --fix` to reformat hundreds of files.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35262202647
    
    Upstream-Ref: Automattic/jetpack@5090b017f1d670095ed6cf41d34feb15aedb20fa

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.3.6-alpha1789670548',
+      'ver' => '0.3.6-alpha1789671588',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.3.1-alpha1789670548',
+      'ver' => '9.3.1-alpha1789671588',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.6.3-alpha1789670548',
+      'ver' => '0.6.3-alpha1789671588',
     ),
     'jetpack-feature-flags' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-feature-flags',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.4.0-alpha1789670548',
+      'ver' => '6.4.0-alpha1789671588',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '8.1.3-alpha1789670548',
+      'ver' => '8.1.3-alpha1789671588',
     ),
     'jetpack-stats-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.5.6-alpha1789670548',
+      'ver' => '0.5.6-alpha1789671588',
     ),
   ),
   'paths' => array(
